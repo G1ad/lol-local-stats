1dc38f8654a627eccdcfa0ff89ac1f013e423614
Raman <romadmukhouski@gmail.com>
2022-11-10 14:06:43 +0100

    alpha 0.0.0.0.2

--- src/main/java/Config/Config.java
+++ src/main/java/Config/Config.java
@@ -3,7 +3,7 @@ package Config;
 public class Config {
 
     public static final String URL_LOCAL = "https://127.0.0.1:2999/liveclientdata/allgamedata";
-    public static final String API_KEY = "RGAPI-d9268003-df24-4b49-a7c6-6c6af6dbe0f0";
+    public static final String API_KEY = "xxxx-xxxx-xxxx-xxxx-xxxx-xxxx";
     public static final String FILE_PATH_ITEMS = "C:\\Users\\gladi\\Desktop\\LoL Stats\\LoL\\items.json";
     public static final String FILE_PATH_CHAMPIONS= "C:\\Users\\gladi\\Desktop\\LoL Stats\\LoL\\Champions.json";
 
